6d6a196c1b267a15521a4a118f1b8beacc0b226d
dreamchasering <happyjun3960@163.com>
2017-06-12 10:28:09 +0800

    常用鼠标、键盘操作

---
+++ test2.py
@@ -0,0 +1,16 @@
+#常用鼠标键盘操作
+# coding=utf-8
+from selenium import webdriver
+
+driver=webdriver.Chrome()
+driver.get("http://www.csdn.net/")
+driver.find_element_by_xpath("//cite[@id='login']/a").click();
+
+driver.find_element_by_id("username").clear()
+driver.find_element_by_id("username").send_keys("name")
+driver.find_element_by_id("password").clear()
+driver.find_element_by_id("password").send_keys("123456")
+#driver.find_element_by_class_name("logging").click()
+driver.find_element_by_class_name("logging").submit()
+
+driver.quit()
