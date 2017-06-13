aa8ff70a676e11d6849a7f517a415a178ea98ca2
dreamchasering <happyjun3960@163.com>
2017-06-13 16:00:42 +0800

    常用鼠标键盘事件

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-#常用鼠标键盘操作
+#常用鼠标键盘事件
 
 from selenium import webdriver
 
@@ -10,7 +10,7 @@ driver.find_element_by_id("username").clear()
 driver.find_element_by_id("username").send_keys("zhangsan")
 driver.find_element_by_id("password").clear()
 driver.find_element_by_id("password").send_keys("123456")
-#driver.find_element_by_class_name("logging").click()
-driver.find_element_by_class_name("logging").submit()
+driver.find_element_by_class_name("logging").click()
+#driver.find_element_by_class_name("logging").submit()
 
 driver.quit()
