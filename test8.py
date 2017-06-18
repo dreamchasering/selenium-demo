bd84582a44a40f08c17fdfec032af451db948f2f
dreamchasering <happyjun3960@163.com>
2017-06-19 07:41:15 +0800

    显式等待1

---
+++ test8.py
@@ -0,0 +1,13 @@
+#显示等待1
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import  expected_conditions as EC
+
+driver=webdriver.Chrome()
+driver.get("http://www.baidu.com")
+
+element=WebDriverWait(driver,5,0.5).until(EC.presence_of_element_located((By.ID,'kw')))
+element.send_keys("Selenium2")
+driver.find_element_by_id("su").click()
+driver.quit()
